e142de8be7a66f9e95537d3856aa3ad26f3d5325
bismarck <bismarckalvess@gmail.com>
2022-03-27 23:30:51 -0300

    Adiciona regra do botão ter tipo button por padrão.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const Button: React.FC<LoadingButtonProps> = ({
   variant = "contained",
+  type="button",
   ...rest
-}) => <LoadingButton variant={variant} {...rest} />;
+}) => <LoadingButton variant={variant} {...rest} type={type} />;
 
 export default Button;
